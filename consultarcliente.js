403745aeafc33e3e5d5593a0c5b544bbad6f6312
carloscruzmt <91795685+carloscruzmt@users.noreply.github.com>
2021-10-12 16:43:45 -0500

    Update consultarcliente.js
    cambio nombre variables

--- consultarcliente.js
+++ consultarcliente.js
@@ -1,12 +1,12 @@
-function consultar(){
+function consultarcliente(){
 
     $.ajax({
-        url: "https://gceec35d4655070-computer.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/computer/computer",
+        url: "https://gada9b0fed1a0c7-reto1.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
         type: 'GET',
         dataType: 'json',
         success: function(respuesta){
             console.log(respuesta.items);
-            mostrarRespuesta(respuesta.items);
+            mostrarRespuesta_cl(respuesta.items);
         },
         error: function (xhr, status) {
             alert('ha sucedido un problema');
@@ -19,7 +19,7 @@ function consultar(){
 
 }
 
-function mostrarRespuesta(items){
+function mostrarRespuesta_cl(items){
    
     var tabla = `<table border="1">
                   <tr>
@@ -46,5 +46,5 @@ function mostrarRespuesta(items){
     }
     tabla +=`</table>`;
 
-    $("#tabla").html(tabla);
+    $("#tabla_cl").html(tabla);
 }
