7d3fdf265261a1e7f2b322d727fa8b9af11c4baf
carloscruzmt <91795685+carloscruzmt@users.noreply.github.com>
2021-10-12 17:12:43 -0500

    Update mes_actualizar.js
    cambio nombre variable y ruta nube oracle

--- mes_actualizar.js
+++ mes_actualizar.js
@@ -2,14 +2,14 @@ function mes_actualizar(){
     console.log("ejecutando funcion para actualizar");
 
     let mensaje = {
-        id: +$("#id").val(),
+        id: +$("#id_msg").val(),
         messagetext:  $("#messagetext").val(),
     };
 
     console.log(mensaje);
 
     $.ajax({
-        url: "https://g2cf131e8b72bef-db202109301853.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/message/message",
+        url: "https://gada9b0fed1a0c7-reto1.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message",
         type: 'PUT',
         dataType: 'json',
         headers: {
